b1cf4b9a519775f87e253784de768748f147a681
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-10 15:13:04 +0330

    add added time field

--- divar/product/models.py
+++ divar/product/models.py
@@ -10,6 +10,8 @@ class Product(models.Model):
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=3)
+    time_added = models.DateTimeField(auto_now_add=True)
+
    
 
 
